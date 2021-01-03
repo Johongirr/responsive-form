604087f4477ec8d9a02e153739e057a7c9b480c3
Johongirr <johongir_rahimov@bk.ru>
2021-01-03 12:36:31 +0100

    added donation price indicator

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+const range = document.querySelector('input[type="range"]');
+const donateAmount = document.querySelector('.donate-amount');
+
+range.addEventListener('input', (e)=>{
+    donateAmount.textContent = `${e.target.value}`;
+})
+
  
